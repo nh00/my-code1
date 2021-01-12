0f2fa7f515b6f582d5a2d21612ce731d856fbe4b
nh00 <haleema.noorbhash@gmail.com>
2021-01-12 16:10:07 +0000

    comment added in the hot fix

--- math.java
+++ math.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
+        // Prints "Hello, World" to the terminal window.This is a comment line
         System.out.println("Hello, World");
     }
 
